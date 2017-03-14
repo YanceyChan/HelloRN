ee01469f385f5e06846e66ef7835e933c8321634
Yancey_Chan <chenyuanshan@outlook1120.com>
2017-03-14 14:36:57 +0800

    自定义组件

--- MyComponent.js
+++ MyComponent.js
@@ -2,14 +2,39 @@ import React, { Component } from 'react';
 import {
     View,
     Text,
+    StyleSheet,
 } from 'react-native';
 
 export default class MyComponent extends Component {
     render () {
         return(
             <View>
-                <Text>MyComponent</Text>
+                <Text style={styles.componentText}>MyComponent</Text>
+                <MySubComponent/>
             </View>
         );
     }
 }
+
+class MySubComponent extends Component {
+    render () {
+        return (
+            <Text style={styles.subComponentText}>subComponent</Text>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    componentText : {
+        textAlign: 'center',
+        color: 'blue',
+        fontWeight: '100',
+        fontSize: 24,
+    },
+    subComponentText : {
+        fontSize: 18,
+        color: '#55ddd9',
+        textAlign: 'center'
+    }
+
+});
