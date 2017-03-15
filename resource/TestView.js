ea2647c5708252b86cc00e70cbbde882fd3a0e5f
Yancey_Chan <chenyuanshan@outlook1120.com>
2017-03-15 11:32:35 +0800

    超级旋转

--- resource/TestView.js
+++ resource/TestView.js
@@ -16,7 +16,50 @@ export default class testView extends Component {
     render() {
         return(
             <View style={myStyles.myView}>
-                <Text style={myStyles.myText}>{this.props.textContent}</Text>
+                <View style={myStyles.topView}>
+                    <View style={myStyles.topSonView1}/>
+                    <View style={myStyles.topSonView2}/>
+                </View>
+                <View style={myStyles.middleView}>
+                    <View style={[myStyles.midSonView, myStyles.defualtPadding]}/>
+                    <View style={[myStyles.midSonView, myStyles.defualtPadding]}/>
+                    <View style={[myStyles.midSonView, myStyles.defualtPadding]}/>
+                    <View style={[myStyles.midSonView, myStyles.defualtPadding]}/>
+                </View>
+                <View style={myStyles.bottomView}>
+                    <View style={{flex:1, backgroundColor: 'green', marginVertical: 5, marginLeft: 5}}/>
+                    <View style={{flex:3,  marginVertical: 5, marginRight: 5}}>
+                        <View style={{flex: 1, backgroundColor: 'green'}}></View>
+                        <View style={{flex: 3, flexDirection: 'row', backgroundColor: '#45ffec'}}>
+                            <View style={{flex: 6, backgroundColor: '#45ffec',}}>
+                                <View style={{flex:5, backgroundColor: '#45ffec', marginLeft: 5,
+                                    marginTop: 5,flexDirection: 'row'}}>
+                                    <View style={{flex: 1, backgroundColor: 'green',}}></View>
+                                    <View style={{flex: 6, backgroundColor: '#45ffec', marginRight: 5, marginBottom: 5}}>
+                                        <View style={{flex: 1, backgroundColor: 'green'}}></View>
+                                        <View style={{flex: 3, backgroundColor: '#45ffec', flexDirection: 'row'}}>
+                                            <View style={{flex: 6, backgroundColor: '#45ffec', marginLeft: 5,
+                                                marginTop: 5}}>
+                                                <View style={{flex: 2, backgroundColor: '#45ffec', flexDirection: 'row',
+                                                    marginRight: 5}}>
+                                                    <View style={{flex: 1, backgroundColor: 'green'}}></View>
+                                                    <View style={{flex: 4, backgroundColor: 'yellow', marginBottom: 5}}>
+                                                        <View style={{flex: 2, backgroundColor: 'green'}}></View>
+                                                        <View style={{flex: 3, backgroundColor: '#45ffec'}}></View>
+                                                    </View>
+                                                </View>
+                                                <View style={{flex: 1, backgroundColor: 'green',}}></View>
+                                            </View>
+                                            <View style={{flex: 1, backgroundColor: 'green'}}></View>
+                                        </View>
+                                    </View>
+                                </View>
+                                <View style={{flex:1, backgroundColor: 'green', marginLeft: 5,}}></View>
+                            </View>
+                            <View style={{flex: 1, backgroundColor: 'green', }}></View>
+                        </View>
+                    </View>
+                </View>
             </View>
         );
     }
@@ -25,15 +68,46 @@ export default class testView extends Component {
 const myStyles = StyleSheet.create({
     myView : {
         flex: 1,
-        backgroundColor: '#ffa63d',
-        justifyContent: 'center',
-        alignItems: 'center',
+        backgroundColor: '#edf4ff',
+
     },
     myText : {
         fontSize: 36,
         color: '#ffffff',
         textAlign: 'center',
         // fontWeight: 400,
+    },
+    topView : {
+        flex: 1,
+        backgroundColor: '#7684ff',
+    },
+    middleView : {
+        flex: 4,
+        backgroundColor: '#ff9eeb',
+        flexDirection: 'row',
+    },
+    bottomView : {
+        flex: 2,
+        backgroundColor: '#45ffec',
+        flexDirection: 'row'
+    },
+    topSonView1 : {
+        flex: 1,
+        margin: 5,
+        backgroundColor: '#ddd1aa',
+    },
+    topSonView2 : {
+        flex: 2,
+        marginHorizontal: 10,
+        backgroundColor: '#ddd1aa',
+    },
+    midSonView : {
+        flex: 1,
+        backgroundColor: 'red'
+    },
+    defualtPadding : {
+        padding: 5,
+        margin: 10,
     }
 
 })
